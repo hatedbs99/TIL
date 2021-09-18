2a5046defdcfce25ae64b6f038f080d089373d23
hatedbs99 <hatedbs99@gmail.com>
2021-09-18 23:10:51 +0900

    21/09/18 | 알고리즘 공부

--- Algorithm_study/백준/tree/test.py
+++ Algorithm_study/백준/tree/test.py
@@ -1,10 +1,3 @@
-def preorder(node):
-    if node == '.':
-        return
-    print(node, end='')
-    preorder(3)
-    preorder(4)
-
-node = {1 : '.'}
-
-preorder(node)
+ans = {}
+ans[1] = 123
+print(ans)
